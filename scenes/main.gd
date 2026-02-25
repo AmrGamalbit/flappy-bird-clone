4bb3c6e45542a85786b6ba3df3314f528dc24be8
Amr Gamal <amrgum2033@gmail.com>
2026-02-25 14:58:11 +0200

    feat: detect collisions with pipes

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+@export var pipe_scene : PackedScene
+
 var game_running : bool
 var game_over : bool
 var scroll
 var score : int
-const SCROLL_SPEED : int = 100
+const SCROLL_SPEED : int = 5
 var screen_size : Vector2i
 var ground_height : int
 var pipes : Array
@@ -13,6 +15,7 @@ const PIPE_RANGE : int = 200
 
 func _ready() -> void:
 	screen_size = get_window().size
+	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
 	new_game()
 	
 func new_game():
@@ -20,6 +23,7 @@ func new_game():
 	game_over = false
 	score = 0
 	scroll = 0
+	generate_pipe()
 	$Bird.reset()
 	
 func _input(event: InputEvent) -> void:
@@ -36,10 +40,37 @@ func start_game():
 	game_running = true
 	$Bird.flying = true
 	$Bird.flap()
-
+	$PipeTimer.start()
+	
 func _process(delta: float) -> void:
 	if game_running: 
-		scroll += SCROLL_SPEED * delta
+		scroll += SCROLL_SPEED
 		if scroll >= screen_size.x:
 			scroll = 0 
 		$Ground.position.x = -scroll
+		
+		for pipe in pipes:
+			pipe.position.x -= SCROLL_SPEED
+
+
+func _on_pipe_timer_timeout() -> void:
+	generate_pipe()
+
+func generate_pipe():
+	var pipe = pipe_scene.instantiate()
+	pipe.position.x = screen_size.x + PIPE_DELAY
+	pipe.position.y = (screen_size.y - ground_height) / 2 + randi_range(-PIPE_RANGE, PIPE_RANGE)
+	pipe.hit.connect(bird_hit)
+	add_child(pipe)
+	pipes.append(pipe)
+
+func bird_hit():
+	$Bird.falling = true
+	stop_game()
+
+
+func stop_game():
+	$PipeTimer.stop()
+	game_running = false
+	game_over = true
+	$Bird.flying = false
